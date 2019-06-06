68f28e2f1c4a3ae9549d01469f64f48581fb1ed8
João Carvalho <joaokvalho@gmail.com>
2019-06-06 16:41:20 -0300

    Components delphi VCL

--- tdi/JcTDIForm.pas
+++ tdi/JcTDIForm.pas
@@ -6,12 +6,6 @@
 { Mail:   joaokvalho@gmail.com                                                 }
 { GitHub: https://github.com/joaokvalho                                        }
 {                                                                              }
-{ Apache License                                                               }
-{ Version 2.0, January 2004                                                    }
-{ http://www.apache.org/licenses/                                              }
-{                                                                              }
-{******************************************************************************}
-
 {******************************************************************************
 |* Historic
 |*
